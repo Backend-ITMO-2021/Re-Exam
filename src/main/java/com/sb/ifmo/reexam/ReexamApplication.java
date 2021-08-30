13d8a81f6521caf309f282099bc1974057efe35b
spolyakovs <sergpolyakow@mail.ru>
2021-08-30 18:02:11 +0300

    Changed properties to env variables

--- src/main/java/com/sb/ifmo/reexam/ReexamApplication.java
+++ src/main/java/com/sb/ifmo/reexam/ReexamApplication.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Calendar;
