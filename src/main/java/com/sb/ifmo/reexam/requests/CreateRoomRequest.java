225f134c493873048b6d3260d50cf1dd42e56ab1
spolyakovs <sergpolyakow@mail.ru>
2021-08-30 19:28:57 +0300

    Fixed creating private room

--- src/main/java/com/sb/ifmo/reexam/requests/CreateRoomRequest.java
+++ src/main/java/com/sb/ifmo/reexam/requests/CreateRoomRequest.java
@@ -16,7 +16,7 @@ public class CreateRoomRequest {
         return isPrivate;
     }
 
-    public void setPrivate(boolean aPrivate) {
-        isPrivate = aPrivate;
+    public void setIsPrivate(boolean isPrivate) {
+        this.isPrivate = isPrivate;
     }
 }
